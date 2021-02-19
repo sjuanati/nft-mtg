912dfd690c3b190fd8be385094bc9734f63253bb
Sergi Juanati <wint3rmute@gmail.com>
2021-02-19 08:25:31 +0100

    Fixed issue with same id contract vs. id token type

--- contracts/MTG.sol
+++ contracts/MTG.sol
@@ -10,7 +10,7 @@ import "./ERC1155/access/Ownable.sol";
  */
 contract MTG is Ownable {
     // ID token counter
-    uint256 id = 1;
+    uint256 idNFT = 1;
 
     // Mapping tokenID => NFT
     mapping(uint256 => NFT) public tokens;
@@ -21,95 +21,107 @@ contract MTG is Ownable {
      */
     function create(string memory _uri) external onlyOwner() {
         // TODO: Check IPFS hash in mapping(uint256 => uint8) to prevent issuing tokens mapped to previous hashes
-        tokens[id] = new NFT(_uri);
-        id += 1;
+        tokens[idNFT] = new NFT(_uri);
+        idNFT += 1;
     }
 
     /**
      * @notice  Mint new tokens within an existing ERC1155 Token
      * @param   account The address of the token holder
-     * @param   _id The ID of the token
+     * @param   _idNFT The ID of the NFT contract
+     * @param   _id The ID of the token type
      * @param   amount The Amount of tokens to be minted
      * @param   data Additional data with no specified format
      */
     function mint(
         address account,
+        uint256 _idNFT,
         uint256 _id,
         uint256 amount,
         bytes memory data
     ) external onlyOwner() {
-        require(address(tokens[_id]) != address(0), "token does not exist");
-        tokens[_id].mint(account, _id, amount, data);
+        require(
+            address(tokens[_idNFT]) != address(0),
+            "token contract does not exist"
+        );
+        tokens[_idNFT].mint(account, _id, amount, data);
     }
 
     /**
      * @notice  Get the URI of the token requested
-     * @param   _id The ID of the token
+     * @param   _idNFT The ID of the NFT contract
+     * @param   _id The ID of the token type
      * @return  The URI of the token requested
      */
-    function uri(uint256 _id) external view returns (string memory) {
-        return tokens[_id].uri(_id);
+    function uri(uint256 _idNFT, uint256 _id)
+        external
+        view
+        returns (string memory)
+    {
+        return tokens[_idNFT].uri(_id);
     }
 
     /**
      * @notice  Get the balance of an account's tokens
      * @param   owner The address of the token holder
-     * @param   _id The ID of the token
+     * @param   _idNFT The ID of the NFT contract
+     * @param   _id The ID of the token type
      * @return  The owner's balance of the token requested
      */
-    function balanceOf(address owner, uint256 _id)
-        external
-        view
-        returns (uint256)
-    {
-        return tokens[_id].balanceOf(owner, _id);
+    function balanceOf(
+        address owner,
+        uint256 _idNFT,
+        uint256 _id
+    ) external view returns (uint256) {
+        return tokens[_idNFT].balanceOf(owner, _id);
     }
 
     /**
      * @notice  Get the address of an account's tokens
-     * @param   _id The ID of the token
+     * @param   _idNFT The ID of the NFT contract
      * @return  The address of the token requested
      */
-    function addressOf(uint256 _id) external view returns (address) {
-        return address(tokens[_id]);
+    function addressOf(uint256 _idNFT) external view returns (address) {
+        return address(tokens[_idNFT]);
     }
 
     /**
      * @notice  Queries the approval status of an operator for a given token holder
      * @param   owner The address of the token holder
      * @param   operator The address of the authorized operator
-     * @param   _id The ID of the token
+     * @param   _idNFT The ID of the NFT contract
      * @return  True if the operator is approved, false if not
      */
     function isApproved(
         address owner,
         address operator,
-        uint256 _id
+        uint256 _idNFT
     ) external view returns (bool) {
-        return tokens[_id].isApprovedForAll(owner, operator);
+        return tokens[_idNFT].isApprovedForAll(owner, operator);
     }
 
     /**
      * @notice  Emergency switch to pause all transfers
-     * @param   _id The ID of the token
+     * @param   _idNFT The ID of the NFT contract
      */
-    function pause(uint256 _id) external onlyOwner() {
-        tokens[_id].pause();
+    function pause(uint256 _idNFT) external onlyOwner() {
+        tokens[_idNFT].pause();
     }
 
     /**
      * @notice  Emergency switch to unpause all transfers
-     * @param   _id The ID of the token
+     * @param   _idNFT The ID of the NFT contract
      */
-    function unpause(uint256 _id) external onlyOwner() {
-        tokens[_id].unpause();
+    function unpause(uint256 _idNFT) external onlyOwner() {
+        tokens[_idNFT].unpause();
     }
 
     /**
      * @notice  Queries the Emergency switch status
+     * @param   _idNFT The ID of the NFT contract
      * @return  True if the contract is paused, false if not
      */
-    function isPaused(uint256 _id) external view returns (bool) {
-        return tokens[_id].paused();
+    function isPaused(uint256 _idNFT) external view returns (bool) {
+        return tokens[_idNFT].paused();
     }
 }
--- test/mtg.js
+++ test/mtg.js
@@ -16,17 +16,19 @@ contract('MTG', (accounts) => {
     });
 
     it('should create NFT', async () => {
-        const balance = await mtg.balanceOf(owner, 1);
-        const uri = await mtg.uri(1);
+        const balance = await mtg.balanceOf(owner, 1, 1);
+        const uri = await mtg.uri(1, 1);
 
         assert(balance.toNumber() === 0);
         assert(uri === 'https://gateway.pinata.cloud/ipfs/QmdsPpTrrWK6256Xpwa9TfbXWo1g3Q6PSYs1jVSQqKSmTS')
     });
 
+    /********************** Minting tokens *************************/ 
+
     it('should mint tokens', async () => {
-        const initialBalance = await mtg.balanceOf(owner, 1);
-        await mtg.mint(owner, 1, 100, text, { from: owner });
-        const finalBalance = await mtg.balanceOf(owner, 1);
+        const initialBalance = await mtg.balanceOf(owner, 1, 1);
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
+        const finalBalance = await mtg.balanceOf(owner, 1, 1);
 
         assert(initialBalance.toNumber() === 0);
         assert(finalBalance.toNumber() === 100);
@@ -37,7 +39,7 @@ contract('MTG', (accounts) => {
         const nft = await NFT.at(addressNFT);
 
         await expectRevert(
-            mtg.mint(user1, 1, 100, text, { from: user1 }),
+            mtg.mint(user1, 1, 1, 100, text, { from: user1 }),
             'Ownable: caller is not the owner -- Reason given: Ownable: caller is not the owner.'
         );
         await expectRevert(
@@ -46,18 +48,20 @@ contract('MTG', (accounts) => {
         );
     });
 
-    it('should NOT mint tokens - token does not exist', async () => {
+    it('should NOT mint tokens - token contract does not exist', async () => {
         await expectRevert(
-            mtg.mint(owner, 4, 100, text, { from: owner }),
-            'token does not exist'
+            mtg.mint(owner, 4, 1, 100, text, { from: owner }),
+            'token contract does not exist'
         );
     });
+
+    /********************** Approving transfers *************************/ 
 
     it('should approve for all', async () => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
 
-        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
         await nft.setApprovalForAll(user1, true, {from: owner});
         const result = await nft.isApprovedForAll(owner, user1);
 
@@ -67,7 +71,7 @@ contract('MTG', (accounts) => {
     it('should NOT approve for all - self approval', async () => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
-        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
     
         await expectRevert(
             nft.setApprovalForAll(owner, true, {from: owner}),
@@ -75,19 +79,21 @@ contract('MTG', (accounts) => {
         );
     });
 
+    /********************** Transferring tokens *************************/ 
+
     it('should transfer tokens to user', async () => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
-        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
 
-        const initialBalanceOwner = await mtg.balanceOf(owner, 1);
-        const initialBalanceUser = await mtg.balanceOf(user1, 1);
+        const initialBalanceOwner = await mtg.balanceOf(owner, 1, 1);
+        const initialBalanceUser = await mtg.balanceOf(user1, 1, 1);
 
         await nft.setApprovalForAll(user1, true, {from: owner});
         await nft.safeTransferFrom(owner, user1, 1, 30, text, { from: owner });
 
-        const finalBalanceOwner = await mtg.balanceOf(owner, 1);
-        const finallBalanceUser = await mtg.balanceOf(user1, 1);
+        const finalBalanceOwner = await mtg.balanceOf(owner, 1, 1);
+        const finallBalanceUser = await mtg.balanceOf(user1, 1, 1);
 
         assert(initialBalanceOwner.toNumber() === 100);
         assert(initialBalanceUser.toNumber() === 0);
@@ -99,7 +105,7 @@ contract('MTG', (accounts) => {
     it('should NOT transfer tokens to user - no approval', async() => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
-        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
 
         await expectRevert(
             nft.safeTransferFrom(owner, user1, 1, 30, text, { from: user1 }),
@@ -110,7 +116,7 @@ contract('MTG', (accounts) => {
     it('should NOT transfer tokens to user - zero address', async() => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
-        await mtg.mint(user1, 1, 100, text, { from: owner });
+        await mtg.mint(user1, 1, 1, 100, text, { from: owner });
 
         await expectRevert(
             nft.safeTransferFrom(user1, ZERO_ADDRESS, 1, 30, text, { from: owner }),
@@ -121,7 +127,7 @@ contract('MTG', (accounts) => {
     it('should NOT transfer tokens to user - insufficient balance', async() => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
-        await mtg.mint(user1, 1, 100, text, { from: owner });
+        await mtg.mint(user1, 1, 1, 100, text, { from: owner });
 
         await expectRevert(
             nft.safeTransferFrom(owner, user1, 1, 200, text, { from: owner }),
@@ -132,10 +138,9 @@ contract('MTG', (accounts) => {
     it('should NOT transfer tokens to user - contract paused', async () => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
-        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
 
         // Check pause
-
         await nft.setApprovalForAll(user1, true, {from: owner});
         await mtg.pause(1);
         let isPaused = await mtg.isPaused(1);
@@ -147,36 +152,33 @@ contract('MTG', (accounts) => {
         );
 
         // Check unpause
-
         await mtg.unpause(1);
         isPaused = await mtg.isPaused(1);
         await nft.safeTransferFrom(owner, user1, 1, 30, text, { from: owner });
-        const balance = await mtg.balanceOf(user1, 1);
+        const balance = await mtg.balanceOf(user1, 1, 1);
 
         assert(isPaused === false);
         assert(balance.toNumber() === 30);
 
     });
 
-    // Test to send ERC1155 tokens to MTG, and they should be lost
-    // Add EIP-165 to prevent stuck tokens in contracts
+    // EIP-165 to prevent stuck tokens in contracts
 
     it('should transfer tokens to contract', async () => {
         const receiver = await RECEIVER.new({ from: owner });
         const addressReceiver = receiver.address;
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
-        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
 
-        const initialBalanceOwner = await mtg.balanceOf(owner, 1);
-        const initialBalanceContract = await mtg.balanceOf(addressReceiver, 1);
+        const initialBalanceOwner = await mtg.balanceOf(owner, 1, 1);
+        const initialBalanceContract = await mtg.balanceOf(addressReceiver, 1, 1);
 
         await nft.setApprovalForAll(addressReceiver, true, {from: owner});
         await nft.safeTransferFrom(owner, addressReceiver, 1, 30, text, { from: owner });
 
-        const finalBalanceOwner = await mtg.balanceOf(owner, 1);
-        const finalBalanceContract = await mtg.balanceOf(addressReceiver, 1);
-
+        const finalBalanceOwner = await mtg.balanceOf(owner, 1, 1);
+        const finalBalanceContract = await mtg.balanceOf(addressReceiver, 1, 1);
 
         assert(initialBalanceOwner.toNumber() === 100);
         assert(initialBalanceContract.toNumber() === 0);
@@ -188,7 +190,7 @@ contract('MTG', (accounts) => {
         const addressNFT =  await mtg.addressOf(1);
         const nft = await NFT.at(addressNFT);
 
-        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 1, 1, 100, text, { from: owner });
         await nft.setApprovalForAll(mtg.address, true, {from: owner});
 
         await expectRevert(
@@ -196,6 +198,19 @@ contract('MTG', (accounts) => {
             'ERC1155: transfer to non ERC1155Receiver implementer'
         );
     });
+/*
+    it.only('should transfer multiple tokens to user', async() => {
+        await mtg.mint(owner, 1, 100, text, { from: owner });
+        await mtg.mint(owner, 2, 200, text, { from: owner });
+        const addressNFT =  await mtg.addressOf(1);
+        const nft = await NFT.at(addressNFT);
+
+        await nft.setApprovalForAll(user1, true, {from: owner});
+
+        //await nft.safeBatchTransferFrom(owner, user1, [1,2], [15, 20], text, {from: owner});
+
+    });
+    */
     
 });
 
